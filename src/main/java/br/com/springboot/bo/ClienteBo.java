037cca808d35b90d693a4e4ad52c30f942ac5a83
Jorge Gabriel <jg3043505@gmail.com>
2026-04-07 16:27:20 -0300

    Feat:CreateForm

--- src/main/java/br/com/springboot/bo/ClienteBo.java
+++ src/main/java/br/com/springboot/bo/ClienteBo.java
@@ -5,6 +5,7 @@ import br.com.springboot.dao.ClienteDao;
 import br.com.springboot.model.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional; // Importação necessária
 
 import java.util.List;
 
@@ -25,28 +26,32 @@ public class ClienteBo implements CRUD<Cliente, Long> {
     }
 
     @Override
+    @Transactional // Abre a transação para inserir
     public void insere(Cliente cliente) {
         dao.insere(cliente);
     }
 
     @Override
+    @Transactional // Abre a transação para atualizar
     public void atualiza(Cliente cliente) {
         dao.atualiza(cliente);
     }
 
     @Override
+    @Transactional // Abre a transação para remover
     public void remove(Cliente cliente) {
         dao.remove(cliente);
     }
 
+    @Transactional // Abre a transação para atualizar (inativar)
     public void inativa(Cliente cliente) {
         cliente.setAtivo(false);
         dao.atualiza(cliente);
     }
 
+    @Transactional // Abre a transação para atualizar (ativar)
     public void ativa(Cliente cliente){
         cliente.setAtivo(true);
         dao.atualiza(cliente);
     }
-
 }
---
+++ src/main/java/br/com/springboot/controller/ClienteController.java
@@ -0,0 +1,48 @@
+package br.com.springboot.controller;
+
+import br.com.springboot.bo.ClienteBo;
+import br.com.springboot.model.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteBo bo;
+
+    @RequestMapping(value = "/novo", method = RequestMethod.GET)
+    public ModelAndView novo(ModelMap model){
+        model.addAttribute("cliente", new Cliente());
+        return new ModelAndView("/cliente/formulario", model);
+    }
+
+
+    @RequestMapping(value = "/salvar", method = RequestMethod.POST)
+    public String salvar(Cliente cliente) {
+        bo.insere(cliente);
+        return "redirect:/clientes/novo";
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ModelAndView lista(ModelMap model){
+        model.addAttribute("clientes", bo.lista());
+        return new ModelAndView("/cliente/lista", model);
+    }
+    @RequestMapping(value = "/editar/{id}", method = RequestMethod.GET)
+    public ModelAndView edita(@PathVariable("id") Long id) {
+        Cliente cliente = bo.pesquisaPeloId(id);
+        ModelMap model = new ModelMap();
+        model.addAttribute("cliente", bo.pesquisaPeloId(id));
+        return new ModelAndView("/cliente/formulario", model);
+    }
+
+
+
+}
